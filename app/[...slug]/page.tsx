9bab2e4d928086be3307eef695add9aa0d012250
DBcoppr <mdtarikhan007@gmail.com>
2025-11-02 01:37:13 +0530

    fix deploy changes

--- app/[...slug]/page.tsx
+++ app/[...slug]/page.tsx
@@ -5,5 +5,5 @@ import dynamic from 'next/dynamic';
 const App = dynamic(() => import('@/frontend/app'), { ssr: false });
 
 export default function Home() {
-  return <App />;
+    return <App />;
 }
