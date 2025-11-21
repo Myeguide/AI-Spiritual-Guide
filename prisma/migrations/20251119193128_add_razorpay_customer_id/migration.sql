c768f36d71e9e1998637bce2b4c4f3b0e580a55e
DBcoppr <mdtarikhan007@gmail.com>
2025-11-21 10:39:50 +0530

    migration change

--- prisma/migrations/20251119193128_add_razorpay_customer_id/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE "payment_methods" ADD COLUMN     "customer_id" TEXT;
-
--- CreateIndex
-CREATE INDEX "payment_methods_customer_id_idx" ON "payment_methods"("customer_id");
